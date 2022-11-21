ed426cefcf4aad916b8fa6da89a036eaf0fba954
Luc4s G4br13l P3r31r4 d4 Cunh4 <91632301+lucas0456@users.noreply.github.com>
2022-11-21 14:40:44 -0300

    atualização do dia 21/10
    coloquei tudo no 000webhostapp.com

--- conexao.php
+++ conexao.php
@@ -1,15 +1,17 @@
 <?php
     //Conexão com o banco
     $local = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $banco = "livraria";
+    $usuario = "id19694321_livrariacli";
+    $senha = "Luc0456sxxx@";
+    $banco = "id19694321_livraria";
 
-    $conexao = mysqli_connect($local, $usuario, $senha, $banco);
+    $conexao2 = mysqli_connect($local, $usuario, $senha, $banco);
 
     header("Content-Type: text/html; charset=utf-8");
+    
   /*  mysqli_query($conexao, "SET NAME 'utf-8'");
     mysqli_query($conexao, "SET character_set_connection = utf8");
     mysqli_query($conexao, "SET character_set_client = utf8");
     mysqli_query($conexao, "SET character_set_result = utf8");*/
+
 ?>
